948502b8b0859e3e70beae010cf4dab4caa5969e
Artem Novichkov <novichkoff93@gmail.com>
2015-07-13 16:02:21 +0600

    BookTableViewCell added

---
+++ Books/Views/BookTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BookTableViewCell.h
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookTableViewCell : UITableViewCell
+
+@end
---
+++ Books/Views/BookTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  BookTableViewCell.m
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import "BookTableViewCell.h"
+
+@implementation BookTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
