1c3351bd9542edfb9f915377fa7693d7b0019936
Artem Novichkov <novichkoff93@gmail.com>
2015-07-13 15:49:12 +0600

    Books model template added

--- Books/Controllers/ViewController.m
+++ Books/Controllers/ViewController.m
@@ -16,7 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _books = [@{} mutableCopy];
+    for (int i = 0; i < 50; i++) {
+        NSMutableArray *book = [@[] mutableCopy];
+        NSInteger chapterCount = arc4random() % 100;
+        for (int j = 0; j < chapterCount; j++) {
+            NSString *chapterTitle = [NSString stringWithFormat:@"Chapter %i", j + 1];
+            [book addObject:chapterTitle];
+        }
+        NSString *bookTitle = [NSString stringWithFormat:@"Book %i", i + 1];
+        self.books[bookTitle] = book;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
