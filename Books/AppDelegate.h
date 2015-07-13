0ab10e3a586ee0075d41304196424f8503f473ab
Artem Novichkov <novichkoff93@gmail.com>
2015-07-13 15:11:05 +0600

    Initial Commit

---
+++ Books/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Books/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
