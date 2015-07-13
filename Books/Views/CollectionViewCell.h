25f26e46acfb36ece51bdf85e8d937c8505787f4
Artem Novichkov <novichkoff93@gmail.com>
2015-07-13 18:17:35 +0600

    CollectionViewCell added

---
+++ Books/Views/CollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.h
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *chapterTitleLabel;
+
+@end
---
+++ Books/Views/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
