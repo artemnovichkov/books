2f604c82408839f7689e7d8017391492dcf2c942
Artem Novichkov <novichkoff93@gmail.com>
2015-07-13 16:26:01 +0600

    CustomCollectionView added

---
+++ Books/Views/CustomCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionView.h
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionView : UICollectionView
+
+@end
---
+++ Books/Views/CustomCollectionView.m
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionView.m
+//  Books
+//
+//  Created by Артем on 13/07/15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+#import "CustomCollectionView.h"
+
+@implementation CustomCollectionView
+
+@end
